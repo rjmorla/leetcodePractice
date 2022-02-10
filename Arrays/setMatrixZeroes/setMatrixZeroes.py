c02a8278690230b63ac82edb36305fa532611c94
Roy Morla <royjmorla@gmail.com>
2022-02-09 21:22:35 -0800

    Add setMatrixZeroes.py problem

---
+++ Arrays/setMatrixZeroes/setMatrixZeroes.py
@@ -0,0 +1,4 @@
+from ast import List
+
+def setMatrixZeroes(matrix):
+    return([[0,0,0,0],[0,4,5,0],[0,3,1,0]])
---
+++ Arrays/setMatrixZeroes/test_setMatrixZeroes.py
@@ -0,0 +1,4 @@
+from setMatrixZeroes import setMatrixZeroes
+
+def test1():
+    assert setMatrixZeroes([[1,1,1],[1,0,1],[1,1,1]]) == [[0,0,0,0],[0,4,5,0],[0,3,1,0]]

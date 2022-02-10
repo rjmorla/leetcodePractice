c4433d7241942ae31b3bdfac86855a9e46e31a30
Roy Morla <royjmorla@gmail.com>
2022-02-09 23:43:22 -0800

    Complete construct2DArray.py solution

--- Arrays/construct2DArray/construct2DArray.py
+++ Arrays/construct2DArray/construct2DArray.py
@@ -1,2 +1,15 @@
-def construct2DArray(original):
-    return 0
+def construct2DArray(original, m, n):
+    # base case m * n invalid matrix size for original 1D array
+    if m * n != len(original):
+        return []
+    
+    # base case for when only 1 row needed in matrix
+    if m == 1:
+        return [original]
+
+    res = []
+    for i in range(m):
+        # [i*m: i*m + n] splicing allows to split into even rows and cols to form matrix
+        res.append(original[i*m: i*m + n]) # i.e. 1-4 is an splice [0:4]
+
+    return res

c644f5bdda3b1059eaa0e58aec40c748b9ad9de9
Roy Morla <royjmorla@gmail.com>
2022-02-09 23:26:25 -0800

    Add additional tests

--- Arrays/construct2DArray/test_construct2DArray.py
+++ Arrays/construct2DArray/test_construct2DArray.py
@@ -1,4 +1,10 @@
 from construct2DArray import construct2DArray
 
 def test_1():
-    assert construct2DArray(0) == 0
+    assert construct2DArray([1,2,3,4], 2, 2) == [[1,2],[3,4]]
+
+def test_2():
+    assert construct2DArray([1,2,3], 1, 3) == [[1,2,3]]
+
+def test_3():
+    assert construct2DArray([1,2], 1, 1) == []

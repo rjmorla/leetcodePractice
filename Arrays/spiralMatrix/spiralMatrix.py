e42c8b3cd771073925ed1cd52f8c197a8183c776
Roy Morla <royjmorla@gmail.com>
2022-02-09 23:15:00 -0800

    Complete spiralMatrix.py problem

--- Arrays/spiralMatrix/spiralMatrix.py
+++ Arrays/spiralMatrix/spiralMatrix.py
@@ -1,3 +1,37 @@
+# solve using a boundary system: Left, Right, Top, and Bottom Boundaries
 def spiralMatrix(matrix):
+    # O(m*n) speed, O(1) space
+    result = []
+    left, right = 0, len(matrix[0])
+    top, bottom = 0, len(matrix)
 
-    return [1,2,3,6,9,8,7,4,5]
+    while left < right and top < bottom:
+        # go left to right first and get every i
+        for i in range(left, right):
+            result.append(matrix[top][i])
+        top += 1
+        
+        # get every i in right most column
+        for i in range(top, bottom):
+            result.append(matrix[i][right - 1])
+        
+        right -= 1
+
+        # catch case where 1d matrices are inserted
+        if not (left < right and top < bottom): 
+            break
+        
+        # get every i in bottom row
+        for i in range(right - 1, left - 1, -1):
+            result.append(matrix[bottom - 1][i])
+        
+        # shifts upwards
+        bottom -= 1
+
+        # get every i in left col
+        for i in range(bottom - 1, top - 1, - 1):
+            result.append(matrix[i][left])
+        
+        left += 1
+
+    return result
--- Arrays/spiralMatrix/test_spiralMatrix.py
+++ Arrays/spiralMatrix/test_spiralMatrix.py
@@ -1,4 +1,4 @@
-from Arrays.spiralMatrix.spiralMatrix import spiralMatrix
+from spiralMatrix import spiralMatrix
 
 def test_1():
     assert spiralMatrix([[1,2,3],[4,5,6],[7,8,9]]) == [1,2,3,6,9,8,7,4,5]

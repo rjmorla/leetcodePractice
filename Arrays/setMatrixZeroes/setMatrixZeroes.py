817146af591699c04879eb47c9c8e46eaa7686eb
Roy Morla <royjmorla@gmail.com>
2022-02-09 22:04:53 -0800

    Complete setMatrixZeroes.py solution

--- Arrays/setMatrixZeroes/setMatrixZeroes.py
+++ Arrays/setMatrixZeroes/setMatrixZeroes.py
@@ -1,4 +1,34 @@
-from ast import List
-
 def setMatrixZeroes(matrix):
-    return([[0,0,0,0],[0,4,5,0],[0,3,1,0]])
+    #O(1)
+    rows, cols = len(matrix), len(matrix[0])
+
+    rowZero = False
+
+    # Get which rows and cols need to be zero
+    for r in range(rows):
+        for c in range(cols):
+            if matrix[r][c] == 0: # if value found in matrix is 0
+                matrix[0][c] = 0 # sets first row in the column to 0
+
+                if r > 0: # prevents setting of top leftmost position to 0
+                    matrix[r][0] = 0 # sets first column in that row to 0
+                else:
+                    rowZero = True
+
+    # set values that aren't first row or first column to 0 if needed
+    # skip first column and row to avoid setting left most column and top most row
+    for r in range(1, rows): 
+        for c in range(1, cols):
+            if matrix[0][c] == 0 or matrix [r][0] == 0:
+                matrix[r][c] = 0
+
+    #zero out first column of matrix
+    if matrix[0][0] == 0:
+        for r in range(rows):
+            matrix[r][0] = 0 
+
+    # zero out first row of matrix
+    if rowZero: 
+        for c in range(cols):
+            matrix[0][c] = 0 
+    return matrix
--- Arrays/setMatrixZeroes/test_setMatrixZeroes.py
+++ Arrays/setMatrixZeroes/test_setMatrixZeroes.py
@@ -1,4 +1,4 @@
 from setMatrixZeroes import setMatrixZeroes
 
 def test1():
-    assert setMatrixZeroes([[1,1,1],[1,0,1],[1,1,1]]) == [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+    assert setMatrixZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]) == [[0,0,0,0],[0,4,5,0],[0,3,1,0]]

92b2aebe5cb67da6971dff4da718afba8ee026af
Roy Morla <royjmorla@gmail.com>
2022-02-12 22:44:55 -0800

    Complete kthLargestElement.py solution

--- heap/kthLargestElement/kthLargestElement.py
+++ heap/kthLargestElement/kthLargestElement.py
@@ -1,3 +1,16 @@
 import heapq
 def getLargestElement(nums, k):
-    return 0
+    if not nums:
+        return None
+
+    maxHeap, res = [], None
+
+    # python is minheap default hence inserting negative num into maxHeap
+    for num in nums: 
+        heapq.heappush(maxHeap, -num)
+
+    # Make sure to add - sign to get original values while popping
+    for _ in range(k):
+        res = -heapq.heappop(maxHeap)
+
+    return res

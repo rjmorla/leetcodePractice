73d07e5248ba8c5a260f2e7271be3be67f0faaf9
Roy Morla <royjmorla@gmail.com>
2022-02-13 20:57:59 -0800

    Complete topKFrequentElements.py Solution

--- heap/topKFrequentElements/topKFrequentElements.py
+++ heap/topKFrequentElements/topKFrequentElements.py
@@ -1,2 +1,19 @@
+import heapq
 def topKFrequent(nums, k):
-    return 0
+    dict, ans = {}, []
+
+    # Make dictionary to keep hash table of value counts in num
+    for num in nums: 
+        if num not in dict:
+            dict[num] = 1
+        else:
+            dict[num] += 1
+
+    # Push to heap
+    for key, value in dict.items():
+        if len(ans) < k:
+            heapq.heappush(ans, [-value, key]) # negative value since python default is minheap
+        else:
+            heapq.heappushpop(ans, [-value, key])
+
+    return (key for value, key in ans) # return only key from value key pairs

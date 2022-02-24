67f33e53aad2672b18a93d4a930806091db41142
Roy Morla <royjmorla@gmail.com>
2022-02-23 18:12:38 -0800

    Fix typo in solution

--- linkedList/reverseLinkedList/reverseLinkedList.py
+++ linkedList/reverseLinkedList/reverseLinkedList.py
@@ -1,7 +1,7 @@
 def reverseLinkedList(head):
     if head is None or head.next is None:
         return head
-    newhead = reverseLinkedList(head.next)
+    newHead = reverseLinkedList(head.next)
     head.next.next = head #sets head of next to point to itself to reverse
     head.next = None
 
